cbdfd457389d32bb89b893a98c14a06ce7430c50
LUNAV <57026491+lunav1346@users.noreply.github.com>
2025-05-31 17:07:44 +0900

    Update object.h

--- object.h
+++ object.h
@@ -11,4 +11,21 @@ typedef struct {
 	bool isHit; // Check Collision, Default is false
 }object;
 
+// function prototype
+// May be Add
+
+
+void spawn_food(object*);
+void draw_food(int, int, char*);
+void check_food(object*);
+
+void spawn_item(object*);
+void draw_item(int, int, char*);
+void check_item(object*);
+
+void spawn_obstacle(object*);
+void draw_obstacle(int, int, char*);
+void check_obstacle(object*);
+
+
 #endif
