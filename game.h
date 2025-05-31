8fedb9bf61c4a699250589c21c27a2c77109dafe
minyong22 <alsdud000403@gmail.com>
2025-05-31 23:34:49 +0900

    Update game.h

--- game.h
+++ game.h
@@ -3,22 +3,17 @@
 
 #include <stdbool.h>
 
-// ==========================
-// 점수 구조체 (가장 위에 배치)
-// ==========================
+
 typedef struct Stat {
     int score;
 } Stat;
 
-// ==========================
-// 화면 크기 (고정)
-// ==========================
+
 #define WIDTH 80 
 #define HEIGHT 24
 
-// ==========================
 // 플레이어 구조체 및 함수
-// ==========================
+
 typedef struct {
     int x;
     int y;
@@ -30,9 +25,8 @@ void spawn_point(player*);
 void draw_point(int, int, char*);
 void check_point(player*);
 
-// ==========================
 // 오브젝트 구조체 및 함수
-// ==========================
+
 typedef struct {
     int x;
     int y;
