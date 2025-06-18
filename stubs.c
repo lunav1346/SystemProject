f6b461f5ea2d05f445d65984aa7b7857c6ddbea6
keon0417 <namgw0512@gmail.com>
2025-06-19 03:23:54 +0900

    Update stubs.c

--- stubs.c
+++ stubs.c
@@ -11,13 +11,13 @@ void draw_food(int y, int x, char* s) {
 }
 
 void check_food(object* o, Stat* s) {}
+//item 부분 수정 ---> item 관련 모두 주석처리
+// void spawn_item(object* o) {
+//     o->x = 13; o->y = 8;
+// }
 
-void spawn_item(object* o) {
-    o->x = 13; o->y = 8;
-}
+// void draw_item(int y, int x, char* s) {
+//     mvprintw(y, x, "%s", s);
+// }
 
-void draw_item(int y, int x, char* s) {
-    mvprintw(y, x, "%s", s);
-}
-
-void check_item(object* o, Stat* s) {}
+// void check_item(object* o, Stat* s) {}
